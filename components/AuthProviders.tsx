064638d756ce6458d61a67b1af8035cf7753df65
axrodriguez <axle@selfmade.com.au>
2023-08-08 16:44:37 +0800

    Upload using cloudinary

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+type Providers = Record<string, Provider>;
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+const AuthProviders = () => {
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      console.log("res: ", res);
+
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <button key={i} title="Sign In" onClick={() => signIn(provider?.id)}>
+            {provider?.id}
+          </button>
+        ))}
+      </div>
+    );
+  }
+};
+
+export default AuthProviders;
